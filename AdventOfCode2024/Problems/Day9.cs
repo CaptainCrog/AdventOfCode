37c97ecdcc787c599ddc83f03caa21451c463ad0
CaptainCrog <craig.penning@gmail.com>
2024-12-10 00:16:33 +0000

    Got Part 1 working, still need to work out part 2

--- AdventOfCode2024/Problems/Day9.cs
+++ AdventOfCode2024/Problems/Day9.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventOfCode2024.Problems
+﻿namespace AdventOfCode2024.Problems
 {
     public class Day9 : DayBase
     {
@@ -12,12 +6,12 @@ namespace AdventOfCode2024.Problems
 
 
         #region Fields
-        string _inputPath = @"C:\Users\craigp\Desktop\AdventOfCode2024PuzzleInputDay9.txt";
-        int _firstResult = 0;
-        int _secondResult = 0;
+        string _inputPath = @"C:\Users\Craig\Desktop\AdventOfCodePuzzleInputs\2024\AdventOfCode2024Day9PuzzleInput.txt";
+        ulong _firstResult = 0;
+        ulong _secondResult = 0;
         int _sum = 0;
         string _diskMap = string.Empty;
-        List<string> _diskMapDecoded = new List<string>();
+        List<(string id, int length)> _diskMapDecoded = new List<(string id, int length)>();
         char[] _antennaFrequencies;
         List<(int row, int col)> _antinodes = new List<(int, int)>();
         #endregion
@@ -35,7 +29,7 @@ namespace AdventOfCode2024.Problems
             }
         }
 
-        int FirstResult
+        ulong FirstResult
         {
             get => _firstResult;
             set
@@ -46,7 +40,7 @@ namespace AdventOfCode2024.Problems
                 }
             }
         }
-        int SecondResult
+        ulong SecondResult
         {
             get => _secondResult;
             set
@@ -80,7 +74,7 @@ namespace AdventOfCode2024.Problems
                 }
             }
         }
-        List<string> DiskMapDecoded
+        List<(string id, int length)> DiskMapDecoded
         {
             get => _diskMapDecoded;
             set
@@ -123,8 +117,8 @@ namespace AdventOfCode2024.Problems
         public Day9()
         {
             InitialiseProblem();
-            FirstResult = SolveFirstProblem<int>();
-            SecondResult = SolveSecondProblem<int>();
+            FirstResult = SolveFirstProblem<ulong>();
+            SecondResult = SolveSecondProblem<ulong>(); //PART 2 = 6390781891880, work back from this to find part 2 solution
             OutputSolution();
         }
         #endregion
@@ -135,22 +129,22 @@ namespace AdventOfCode2024.Problems
             DiskMap = File.ReadAllText(_inputPath).Replace("\r", string.Empty).Replace("\n", string.Empty);
             bool isFile = true;
             int id = 0;
-            foreach(var numberChar in DiskMap)
+            foreach (var numberChar in DiskMap)
             {
-                int number = int.Parse(numberChar.ToString());
+                int length = int.Parse(numberChar.ToString());
                 if (isFile)
                 {
-                    for (int i = 0; i <= number - 1; i++)
+                    for (int i = 0; i <= length - 1; i++)
                     {
-                        DiskMapDecoded.Add(id.ToString());
+                        DiskMapDecoded.Add((id.ToString(), length));
                     }
                     id++;
                 }
                 else
                 {
-                    for (int i = 0; i <= number - 1; i++)
+                    for (int i = 0; i <= length - 1; i++)
                     {
-                        DiskMapDecoded.Add(".");
+                        DiskMapDecoded.Add((".", length));
                     }
                 }
                 isFile = !isFile;
@@ -159,16 +153,16 @@ namespace AdventOfCode2024.Problems
 
         public override T SolveFirstProblem<T>()
         {
-            Sum = 0;
-            SortDiskMap();
-            Sum = CalculateDiskMap();
+            var diskMapDecodedPart1Copy = SortDiskMapFragmented();
+            var result = CalculateDiskMap(diskMapDecodedPart1Copy);
 
-            return (T)Convert.ChangeType(Sum, typeof(T));
+            return (T)Convert.ChangeType(result, typeof(T));
         }
         public override T SolveSecondProblem<T>()
         {
-            Sum = 0;
-            return (T)Convert.ChangeType(Sum, typeof(T));
+            var disMapDecodedPart2Copy = SortDiskMapUnfragmented();
+            var result = CalculateDiskMap(disMapDecodedPart2Copy);
+            return (T)Convert.ChangeType(result, typeof(T));
         }
 
         public override void OutputSolution()
@@ -177,21 +171,22 @@ namespace AdventOfCode2024.Problems
             Console.WriteLine($"Second Solution is: {SecondResult}");
         }
 
-        private void SortDiskMap()
+        private List<(string id, int length)> SortDiskMapFragmented()
         {
             int reversedIndex = DiskMapDecoded.Count() - 1;
+            var diskMapDecodedPart1Copy = DiskMapDecoded.ToList();
 
-            for (int i = 0; i <= DiskMapDecoded.Count() - 1; i++)
+            for (int i = 0; i <= diskMapDecodedPart1Copy.Count() - 1; i++)
             {
-                if (DiskMapDecoded[i] == ".")
+                if (diskMapDecodedPart1Copy[i].id == ".")
                 {
-                    for (int j = reversedIndex; j >= i+1; j--)
+                    for (int j = reversedIndex; j >= i + 1; j--)
                     {
-                        if (DiskMapDecoded[j] != ".")
+                        if (diskMapDecodedPart1Copy[j].id != ".")
                         {
-                            var value = DiskMapDecoded[j];
-                            DiskMapDecoded[j] = ".";
-                            DiskMapDecoded[i] = value;
+                            var value = diskMapDecodedPart1Copy[j];
+                            diskMapDecodedPart1Copy[j] = diskMapDecodedPart1Copy[i];
+                            diskMapDecodedPart1Copy[i] = value;
                             reversedIndex = j;
                             break;
                         }
@@ -200,18 +195,161 @@ namespace AdventOfCode2024.Problems
                     }
                 }
             }
+
+            return diskMapDecodedPart1Copy;
         }
 
-        private int CalculateDiskMap()
+        private List<(string id, int length)> SortDiskMapUnfragmented2()
         {
-            int sum = 0;
-            DiskMapDecoded = DiskMapDecoded.Where(x => x != ".").ToList();
-            for (int i = 0; i <= DiskMapDecoded.Count()-1; i++)
+            var diskMapDecodedPart1Copy = DiskMapDecoded.ToList();
+
+            for (int i = 0; i <= diskMapDecodedPart1Copy.Count() - 1; i++)
             {
-                sum += (int.Parse(DiskMapDecoded[i]) * i);
+                if (diskMapDecodedPart1Copy[i].id == ".")
+                {
+                    for (int j = diskMapDecodedPart1Copy.Count - 1; j >= i + 1; j--)
+                    {
+                        if (diskMapDecodedPart1Copy[j].id != ".")
+                        {
+                            if (diskMapDecodedPart1Copy[i].length >= diskMapDecodedPart1Copy[j].length)
+                            {
+                                int leftOverFreeSpaceLength = 0;
+                                var temp = diskMapDecodedPart1Copy[j];
+                                if (diskMapDecodedPart1Copy[i].length - diskMapDecodedPart1Copy[j].length != 0)
+                                {
+                                    leftOverFreeSpaceLength = diskMapDecodedPart1Copy[i].length - diskMapDecodedPart1Copy[j].length;
+                                    for (int k = diskMapDecodedPart1Copy[j].length; k <= diskMapDecodedPart1Copy[i].length; k++)
+                                    {
+                                        diskMapDecodedPart1Copy[i + k] = new(diskMapDecodedPart1Copy[i + k].id, leftOverFreeSpaceLength);
+                                    }
+                                }
+
+
+                                for (int k = 0; k <= diskMapDecodedPart1Copy[i].length - 1; k++)
+                                {
+                                    var value = diskMapDecodedPart1Copy[j - k];
+                                    diskMapDecodedPart1Copy[j - k] = diskMapDecodedPart1Copy[i + k];
+                                    diskMapDecodedPart1Copy[i + k] = value;
+                                }
+                                break;
+                            }
+
+                        }
+                        else
+                        {
+                            continue;
+                        }
+
+                    }
+                }
             }
-            return sum;
-            //1945531743 TOO LOW
+
+            return diskMapDecodedPart1Copy;
+        }
+
+        private List<(string id, int length)> SortDiskMapUnfragmented()
+        {
+            var diskMapDecodedPart1Copy = DiskMapDecoded.ToList();
+            //int i = 0; i <= diskMapDecodedPart1Copy.Count() - 1; i++
+            for (int i = diskMapDecodedPart1Copy.Count() - 1; i >= 0; i--)
+            {
+                if (diskMapDecodedPart1Copy[i].id != ".")
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        if (diskMapDecodedPart1Copy[j].id == ".")
+                        {
+                            if (diskMapDecodedPart1Copy[j].length >= diskMapDecodedPart1Copy[i].length)
+                            {
+                                int leftOverFreeSpaceLength = 0;
+                                var temp = diskMapDecodedPart1Copy[i];
+                                if (diskMapDecodedPart1Copy[j].length - diskMapDecodedPart1Copy[i].length != 0)
+                                {
+                                    leftOverFreeSpaceLength = diskMapDecodedPart1Copy[j].length - diskMapDecodedPart1Copy[i].length;
+                                    for (int k = diskMapDecodedPart1Copy[i].length; k <= diskMapDecodedPart1Copy[j].length; k++)
+                                    {
+                                        diskMapDecodedPart1Copy[j + k] = new(diskMapDecodedPart1Copy[j + k].id, leftOverFreeSpaceLength);
+                                    }
+                                }
+
+
+                                for (int k = 0; k <= diskMapDecodedPart1Copy[j].length - 1; k++)
+                                {
+                                    var value = diskMapDecodedPart1Copy[i - k];
+                                    diskMapDecodedPart1Copy[i - k] = diskMapDecodedPart1Copy[j + k];
+                                    diskMapDecodedPart1Copy[j + k] = value;
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+
+
+
+
+
+                //    if (diskMapDecodedPart1Copy[i].id == ".")
+                //{
+                //    for (int j = diskMapDecodedPart1Copy.Count - 1; j >= i + 1; j--)
+                //    {
+                //        if (diskMapDecodedPart1Copy[j].id != ".")
+                //        {
+                //            if (diskMapDecodedPart1Copy[i].length >= diskMapDecodedPart1Copy[j].length)
+                //            {
+                //                int leftOverFreeSpaceLength = 0;
+                //                var temp = diskMapDecodedPart1Copy[j];
+                //                if (diskMapDecodedPart1Copy[i].length - diskMapDecodedPart1Copy[j].length != 0)
+                //                {
+                //                    leftOverFreeSpaceLength = diskMapDecodedPart1Copy[i].length - diskMapDecodedPart1Copy[j].length;
+                //                    for (int k = diskMapDecodedPart1Copy[j].length; k <= diskMapDecodedPart1Copy[i].length; k++)
+                //                    {
+                //                        diskMapDecodedPart1Copy[i + k] = new(diskMapDecodedPart1Copy[i + k].id, leftOverFreeSpaceLength);
+                //                    }
+                //                }
+
+
+                //                for (int k = 0; k <= diskMapDecodedPart1Copy[i].length - 1; k++)
+                //                {
+                //                    var value = diskMapDecodedPart1Copy[j - k];
+                //                    diskMapDecodedPart1Copy[j - k] = diskMapDecodedPart1Copy[i + k];
+                //                    diskMapDecodedPart1Copy[i + k] = value;
+                //                }
+                //                break;
+                //            }
+
+                //        }
+                //        else
+                //        {
+                //            continue;
+                //        }
+
+                //    }
+                //}
+            }
+
+            return diskMapDecodedPart1Copy;
+        }
+
+        private ulong CalculateDiskMap(List<(string id, int length)> diskMapDecodedPart1Copy)
+    {
+        ulong checkSum = 0;
+        for (int i = 0; i <= diskMapDecodedPart1Copy.Count() - 1; i++)
+        {
+            if (ulong.TryParse(diskMapDecodedPart1Copy[i].id, out ulong number))
+            {
+                checkSum += number * (ulong)i;
+            }
+        }
+        return checkSum;
+            //6367087064415	THATS A BINGO
+
+            //6390216683958 PART 2 TOO LOW
+            //6390682211954	TOO LOW
+            //6390781741404
+            //6389514248351
+
+
         }
 
         #endregion
