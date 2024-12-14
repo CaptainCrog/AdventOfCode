382b691342164e93e9d712613a7446fe8660f668
CaptainCrog <craig.penning@gmail.com>
2024-12-14 01:23:15 +0000

    Started Part 2 implementation, maths is whack at the moment. Rework in the morning

--- AdventOfCode2024/Problems/Day13.cs
+++ AdventOfCode2024/Problems/Day13.cs
@@ -10,8 +10,8 @@ namespace AdventOfCode2024.Problems
     public partial class Day13 : DayBase
     {
         #region Fields
-        //string _inputPath = @"C:\Users\Craig\Desktop\AdventOfCodePuzzleInputs\2024\TestInputs\AdventOfCode2024Day13TestInput.txt";
-        string _inputPath = @"C:\Users\Craig\Desktop\AdventOfCodePuzzleInputs\2024\PuzzleInputs\AdventOfCode2024Day13PuzzleInput.txt";
+        string _inputPath = @"C:\Users\Craig\Desktop\AdventOfCodePuzzleInputs\2024\TestInputs\AdventOfCode2024Day13TestInput.txt";
+        //string _inputPath = @"C:\Users\Craig\Desktop\AdventOfCodePuzzleInputs\2024\PuzzleInputs\AdventOfCode2024Day13PuzzleInput.txt";
         long _firstResult = 0;
         ulong _secondResult = 0;
         string[] _gardenPlot = [];
@@ -173,12 +173,13 @@ namespace AdventOfCode2024.Problems
 
             foreach (var clawMachine in ClawMachines)
             {
-                (int aPresses, int bPresses) presses = BreadthFirstSearch(clawMachine);
+                (long aPresses, long bPresses) presses = CalculateButtonPresses(clawMachine);
                 if (presses != (-1, -1))
                     sum += (presses.aPresses * _buttonAToken + presses.bPresses * _buttonBToken);
             }
 
-            return (T)Convert.ChangeType(0, typeof(T));
+            return (T)Convert.ChangeType(sum, typeof(T));
+            // Test input should sum up to 875318608908
         }
 
         (int buttonAPressedCount, int buttonBPressedCount) BreadthFirstSearch(ClawMachine clawMachine)
@@ -201,6 +202,95 @@ namespace AdventOfCode2024.Problems
             }
 
             return (-1, -1);
+        }
+
+        (long xResult, long yResult) CalculateButtonPresses(ClawMachine clawMachine)
+        {
+            // Check feasibility of each equation
+            var gcd_x = FindGreatestCommonDiviser(clawMachine.ButtonA.XMovement, clawMachine.ButtonB.XMovement);
+            var gcd_y = FindGreatestCommonDiviser(clawMachine.ButtonA.YMovement, clawMachine.ButtonB.YMovement);
+
+            if (clawMachine.Prize.xValue % gcd_x != 0 || clawMachine.Prize.yValue % gcd_y != 0)
+                return (-1, -1);
+
+            (long diophantineButtonAX, long diophantineButtonBX) = SolveDiophantine(clawMachine.ButtonA.XMovement, clawMachine.ButtonB.XMovement, clawMachine.Prize.xValue, gcd_x);
+            (long diophantineButtonAY, long diophantineButtonBY) = SolveDiophantine(clawMachine.ButtonA.YMovement, clawMachine.ButtonB.YMovement, clawMachine.Prize.yValue, gcd_y);
+
+            (diophantineButtonAX, diophantineButtonBX) = AdjustForNonNegative(diophantineButtonAX, diophantineButtonBX, clawMachine.ButtonA.XMovement, clawMachine.ButtonB.XMovement, gcd_x);
+            (diophantineButtonAY, diophantineButtonBY) = AdjustForNonNegative(diophantineButtonAY, diophantineButtonBY, clawMachine.ButtonA.YMovement, clawMachine.ButtonB.YMovement, gcd_y);
+
+            (long xResult, long yResult) result = CombineEquations(diophantineButtonAX, diophantineButtonBX, diophantineButtonAY, diophantineButtonBY, clawMachine.ButtonA.XMovement, clawMachine.ButtonB.XMovement, clawMachine.ButtonA.YMovement, clawMachine.ButtonB.YMovement);
+
+            return result;
+        }
+
+
+        long FindGreatestCommonDiviser(long buttonAValue, long buttonBValue)
+        {
+            while (buttonBValue != 0)
+            {
+                var temp = buttonBValue;
+                buttonBValue = buttonAValue % buttonBValue;
+                buttonAValue = temp;
+
+            }
+            return buttonAValue;
+        }
+
+        (long diophantineButtonAValue, long diophantineButtonBValue) SolveDiophantine(long buttonAMovement, long buttonBMovement, long prizeValue, long gcd)
+        {
+            (long x, long y) = GetExtendedEuclidean(buttonAMovement, buttonBMovement);
+            long scale = prizeValue / gcd;
+
+            var gcd_abx = x * scale;
+            var gcd_aby = y * scale;
+            return (gcd_abx, gcd_aby);
+        }
+
+        (long extendedEuclideanA, long extendedEuclideanB) GetExtendedEuclidean(long buttonAMovement, long buttonBMovement)
+        {
+            if (buttonBMovement == 0)
+                return (1, 0);
+            (long x1, long y1) = GetExtendedEuclidean(buttonBMovement, buttonAMovement % buttonBMovement);
+            return (y1, x1 - (buttonAMovement / buttonBMovement) * y1);
+        }
+        (long adjustedDiophantineButtonAValue, long adjustedDiophantineButtonBValue) AdjustForNonNegative(long diophantineButtonAValue, long diophantineButtonBValue, long buttonAMovement, long buttonBMovement, long gcd)
+        {
+            long k_min_a = (diophantineButtonAValue < 0) ? (long)Math.Ceiling(-diophantineButtonAValue / (double)(buttonAMovement)) : 0;
+            long k_min_b = (diophantineButtonBValue < 0) ? (long)Math.Ceiling(-diophantineButtonBValue / (double)(buttonBMovement)) : 0;
+            long k = Math.Max(k_min_a, k_min_b);
+
+
+            //var k_min = Math.Ceiling((decimal)(-diophantineButtonAValue * gcd / diophantineButtonBValue));
+            //var k = Math.Max(k_min, Math.Ceiling((decimal)(diophantineButtonBValue * gcd / diophantineButtonAValue)));
+            diophantineButtonAValue += (long)k * (buttonBMovement / gcd);
+            diophantineButtonBValue += (long)k * (buttonAMovement / gcd);
+
+            return (diophantineButtonAValue, diophantineButtonBValue);
+        }
+
+        (long, long) CombineEquations(long diophantineButtonAX, long diophantineButtonBX, long diophantineButtonAY, long diophantineButtonBY, long buttonAXMovement, long buttonBXMovement, long buttonAYMovement, long buttonBYMovement)
+        {
+            if (diophantineButtonAX == diophantineButtonAY && diophantineButtonBX == diophantineButtonBY)
+            {
+                return (diophantineButtonAX, diophantineButtonBX);
+            }
+
+            long diff_n_a = diophantineButtonAY - diophantineButtonAX;
+            long gcd = FindGreatestCommonDiviser(buttonBXMovement, buttonBYMovement); 
+            if (diff_n_a % gcd != 0)
+                return (-1, -1);
+            (long x, long y) = GetExtendedEuclidean(buttonBXMovement, buttonBYMovement);
+            long k = x * (diff_n_a / gcd);
+
+            //var k = (diophantineButtonAY - diophantineButtonAX) / (buttonAXMovement * buttonBYMovement - buttonBXMovement * buttonAYMovement);
+            var buttonACombined = diophantineButtonAX + k * buttonAXMovement;
+            var buttonBCombined = diophantineButtonBX + k * buttonBXMovement;
+
+            if (buttonACombined < 0 || buttonBCombined < 0)
+                return (-1, -1);
+
+            return (buttonACombined, buttonBCombined);
         }
 
         #endregion
