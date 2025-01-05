166ff7b0126df73d396c59ed497bd3d64aa29c62
CaptainCrog <craig.penning@gmail.com>
2025-01-05 23:46:50 +0000

    Implemented Day 5 Solution

--- AdventOfCode2015/Problems/Day5.cs
+++ AdventOfCode2015/Problems/Day5.cs
@@ -2,18 +2,14 @@
 
 namespace AdventOfCode2015.Problems
 {
-    public class Day5 : DayBase
+    public partial class Day5 : DayBase
     {
         #region Fields
 
         string _inputPath = string.Empty;
-        List<string> _pageOrderingRules = new();
-        List<string> _pagesToProduce = new();
-        List<string> _invalidPages = new();
-        Dictionary<int, List<int>> _groupedRules = new();
         int _firstResult = 0;
         int _secondResult = 0;
-        int _sum = 0;
+        string[] _textFile = [];
 
         #endregion
 
@@ -54,62 +50,6 @@ namespace AdventOfCode2015.Problems
             }
         }
 
-        int Sum
-        {
-            get => _sum;
-            set
-            {
-                if (_sum != value)
-                {
-                    _sum = value;
-                }
-            }
-        }
-        List<string> PageOrderingRules
-        {
-            get => _pageOrderingRules;
-            set
-            {
-                if (_pageOrderingRules != value)
-                {
-                    _pageOrderingRules = value;
-                }
-            }
-        }
-        List<string> PagesToProduce
-        {
-            get => _pagesToProduce;
-            set
-            {
-                if (_pagesToProduce != value)
-                {
-                    _pagesToProduce = value;
-                }
-            }
-        }
-
-        List<string> InvalidPages
-        {
-            get => _invalidPages;
-            set
-            {
-                if (_invalidPages != value)
-                {
-                    _invalidPages = value;
-                }
-            }
-        }
-        Dictionary<int, List<int>> GroupedRules
-        {
-            get => _groupedRules;
-            set
-            {
-                if (_groupedRules != value)
-                {
-                    _groupedRules = value;
-                }
-            }
-        }
         #endregion
 
         #region Constructor
@@ -126,25 +66,7 @@ namespace AdventOfCode2015.Problems
         #region Methods
         public override void InitialiseProblem()
         {
-            var rawInputData = File.ReadAllLines(_inputPath);
-            PageOrderingRules = rawInputData.Where(x => x.Contains('|')).ToList();
-            PagesToProduce = rawInputData.Where(x => x.Contains(',')).ToList();
-
-
-            Regex keyRegex = new Regex(@"[0-9]{2}");
-            Regex groupRegex = new Regex(@"\|[0-9]{2}");
-            var allRules = new List<Match>();
-            var ruleValues = PageOrderingRules.Select(rule => keyRegex.Match(rule).Value).Distinct().ToList();
-            foreach (var rule in ruleValues)
-            {
-                var ruleGroup = PageOrderingRules.Where(x => x.Contains($"{rule}|")).ToList();
-                var dictGroup = new List<int>();
-                foreach (var value in ruleGroup)
-                {
-                    dictGroup.Add(int.Parse(groupRegex.Match(value).Value.Substring(1)));
-                }
-                GroupedRules.Add(int.Parse(rule), dictGroup);
-            }
+            _textFile = File.ReadAllLines(_inputPath);
 
         }
 
@@ -156,75 +78,57 @@ namespace AdventOfCode2015.Problems
 
         public override T SolveFirstProblem<T>()
         {
-            Sum = 0;
-            ProcessPages();
-            return (T)Convert.ChangeType(Sum, typeof(T));
+            var niceStringsCount = 0;
+            var vowelRegex = VowelRegex();
+            var doubleLetterRegex = DoubleLetterRegex();
+            var invalidStringRegex = InvalidStringRegex();
+            foreach ( var line in _textFile ) 
+            {
+                var containsAtLeast3Vowels = vowelRegex.Matches( line ).Count >= 3;
+                var containsAtLeast1DoubleCharacter = doubleLetterRegex.Matches(line).Count >= 1;
+                var containsNoInvalidStrings = invalidStringRegex.Matches( line ).Count == 0;
+
+                if (containsAtLeast3Vowels && containsAtLeast1DoubleCharacter && containsNoInvalidStrings)
+                    niceStringsCount++;
+            }
+
+
+            return (T)Convert.ChangeType(niceStringsCount, typeof(T));
         }
 
         public override T SolveSecondProblem<T>()
         {
-            Sum = 0;
-            FixPages();
-            return (T)Convert.ChangeType(Sum, typeof(T));
-        }
-
-
-        int ProcessPages()
-        {
-            foreach(var page in PagesToProduce)
+            var niceStringsCount = 0;
+            var repeatedStringRegex = RepeatedStringRegex();
+            var singleLetterRepeatedRegex = SingleLetterRepeatedAfterAnotherLetter();
+            foreach (var line in _textFile)
             {
-                var pageInvalid = false;
-                var numbers = page.Split(',').Select(int.Parse).ToList();
-                var middleNumber = numbers[numbers.Count / 2];
-                for (int i = 0; i < numbers.Count; i++) 
-                {
-                    if (pageInvalid)
-                        break;
+                var containsAtLeast1Pair = repeatedStringRegex.Matches(line).Count >= 1;
+                var containsAtLeast1RepeatedLetter = singleLetterRepeatedRegex.Matches(line).Count >= 1;
 
-                    GroupedRules.TryGetValue(numbers[i], out var group);
-                    for (int j = i + 1; j < numbers.Count; j++)
-                    {
-                        if (group == null || !group.Contains(numbers[j]))
-                        {
-                            pageInvalid = true;
-                            InvalidPages.Add(page);
-                            break;
-                        }
-                    }
-                }
-                if (!pageInvalid)
-                    Sum += middleNumber;
+                if (containsAtLeast1Pair && containsAtLeast1RepeatedLetter)
+                    niceStringsCount++;
             }
-            return Sum;
+
+
+            return (T)Convert.ChangeType(niceStringsCount, typeof(T));
         }
 
-        void FixPages()
-        {
-            foreach(var invalidPage in InvalidPages)
-            {
-                var numbers = invalidPage.Split(',').Select(int.Parse).ToList();
-                var allPageGroups = GroupedRules.Where(x => numbers.Contains(x.Key));
-                var numberCorrectPositions = new List<(int number, int corrrectPosition)>();
-                var fixedPage = new List<int>();
-                for (int i = 0; i < numbers.Count; i++)
-                {
-                    var groups = allPageGroups.Select(x => x.Value).ToList();
-                    var numberCorrectPosition = -1; //Start off negative as each iteration will ammend the position by 1
-                    foreach (var group in groups)
-                    {
-                        if (group.Contains(numbers[i]))
-                            numberCorrectPosition++;
-                    }
 
-                    numberCorrectPositions.Add((numbers[i], numberCorrectPosition));
-                }
-                foreach (var fix in numberCorrectPositions.OrderByDescending(x => x.corrrectPosition))
-                {
-                    fixedPage.Add(fix.number);
-                }
-                Sum += fixedPage[fixedPage.Count / 2];
-            }
-        }
+        [GeneratedRegex(@"a|e|i|o|u")]
+        private static partial Regex VowelRegex();
+
+        [GeneratedRegex(@"([a-z]{1})\1")]
+        private static partial Regex DoubleLetterRegex();
+
+        [GeneratedRegex(@"ab|cd|pq|xy")]
+        private static partial Regex InvalidStringRegex();
+
+        [GeneratedRegex(@"(..).*\1")]
+        private static partial Regex RepeatedStringRegex();
+
+        [GeneratedRegex(@"(.).\1")]
+        private static partial Regex SingleLetterRepeatedAfterAnotherLetter();
 
         #endregion
     }
--- AdventOfCode2015Tests/Day5.cs
+++ AdventOfCode2015Tests/Day5.cs
@@ -5,18 +5,123 @@ namespace AdventOfCode2015Tests
     {
         [TestMethod]
         [DeploymentItem("Inputs/Day5/Day5Test1.txt")]
-        public void Part1_ProducesCorrectTotalOfAlreadyOrderedMiddlePageNumbers_IsTrue()
+        public void Part1_CorrectlyAssumesStringIsNaughtyFile1_IsTrue()
         {
             var instance = new AdventOfCode2015.Problems.Day5("Day5Test1.txt");
-            instance.FirstResult.Should().Be(143);
+            instance.FirstResult.Should().Be(0);
+        }
+        [TestMethod]
+        [DeploymentItem("Inputs/Day5/Day5Test2.txt")]
+        public void Part1_CorrectlyAssumesStringIsNiceFile2_IsTrue()
+        {
+            var instance = new AdventOfCode2015.Problems.Day5("Day5Test2.txt");
+            instance.FirstResult.Should().Be(1);
+        }
+        [TestMethod]
+        [DeploymentItem("Inputs/Day5/Day5Test3.txt")]
+        public void Part1_CorrectlyAssumesStringIsNaughtyFile3_IsTrue()
+        {
+            var instance = new AdventOfCode2015.Problems.Day5("Day5Test3.txt");
+            instance.FirstResult.Should().Be(0);
+        }
+        [TestMethod]
+        [DeploymentItem("Inputs/Day5/Day5Test4.txt")]
+        public void Part1_CorrectlyAssumesStringIsNaughtyFile4_IsTrue()
+        {
+            var instance = new AdventOfCode2015.Problems.Day5("Day5Test4.txt");
+            instance.FirstResult.Should().Be(0);
+        }
+        [TestMethod]
+        [DeploymentItem("Inputs/Day5/Day5Test5.txt")]
+        public void Part1_CorrectlyAssumesStringIsNaughtyFile5_IsTrue()
+        {
+            var instance = new AdventOfCode2015.Problems.Day5("Day5Test5.txt");
+            instance.FirstResult.Should().Be(0);
+        }
+        [TestMethod]
+        [DeploymentItem("Inputs/Day5/Day5Test6.txt")]
+        public void Part1_CorrectlyAssumesStringIsNiceFile6_IsTrue()
+        {
+            var instance = new AdventOfCode2015.Problems.Day5("Day5Test6.txt");
+            instance.FirstResult.Should().Be(1);
+        }
+        [TestMethod]
+        [DeploymentItem("Inputs/Day5/Day5Test7.txt")]
+        public void Part1_CorrectlyAssumesStringIsNaughtyFile7_IsTrue()
+        {
+            var instance = new AdventOfCode2015.Problems.Day5("Day5Test7.txt");
+            instance.FirstResult.Should().Be(0);
+        }
+        [TestMethod]
+        [DeploymentItem("Inputs/Day5/Day5Test8.txt")]
+        public void Part1_CorrectlyAssumesStringIsNaughtyFile8_IsTrue()
+        {
+            var instance = new AdventOfCode2015.Problems.Day5("Day5Test8.txt");
+            instance.FirstResult.Should().Be(0);
+        }
+        [TestMethod]
+        [DeploymentItem("Inputs/Day5/Day5Test9.txt")]
+        public void Part1_CorrectlyAssumesStringIsNiceFile9_IsTrue()
+        {
+            var instance = new AdventOfCode2015.Problems.Day5("Day5Test9.txt");
+            instance.FirstResult.Should().Be(1);
+        }
+        [TestMethod]
+        [DeploymentItem("Inputs/Day5/Day5Test10.txt")]
+        public void Part1_CorrectlyAssumesStringIsNaughtyFile10_IsTrue()
+        {
+            var instance = new AdventOfCode2015.Problems.Day5("Day5Test10.txt");
+            instance.FirstResult.Should().Be(0);
+        }
+        [TestMethod]
+        [DeploymentItem("Inputs/Day5/Day5Test11.txt")]
+        public void Part1_CorrectlyAssumesStringIsNaughtyFile11_IsTrue()
+        {
+            var instance = new AdventOfCode2015.Problems.Day5("Day5Test11.txt");
+            instance.FirstResult.Should().Be(0);
+        }
+        [TestMethod]
+        [DeploymentItem("Inputs/Day5/Day5Test12.txt")]
+        public void Part1_CorrectlyAssumesStringIsNaughtyFile12_IsTrue()
+        {
+            var instance = new AdventOfCode2015.Problems.Day5("Day5Test12.txt");
+            instance.FirstResult.Should().Be(0);
+        }
+        [TestMethod]
+        [DeploymentItem("Inputs/Day5/Day5Test13.txt")]
+        public void Part1_CorrectlyAssumesStringIsNiceFile13_IsTrue()
+        {
+            var instance = new AdventOfCode2015.Problems.Day5("Day5Test13.txt");
+            instance.FirstResult.Should().Be(1);
         }
 
         [TestMethod]
-        [DeploymentItem("Inputs/Day5/Day5Test1.txt")]
-        public void Part2_ProducesCorrectTotalOfIncorrectlyOrderedMiddlePageNumbersAfterModification_IsTrue()
+        [DeploymentItem("Inputs/Day5/Day5Test14.txt")]
+        public void Part2_CorrectlyAssumesStringIsNiceFile14_IsTrue()
         {
-            var instance = new AdventOfCode2015.Problems.Day5("Day5Test1.txt");
-            instance.SecondResult.Should().Be(123);
+            var instance = new AdventOfCode2015.Problems.Day5("Day5Test14.txt");
+            instance.SecondResult.Should().Be(1);
+        }
+        [TestMethod]
+        [DeploymentItem("Inputs/Day5/Day5Test15.txt")]
+        public void Part2_CorrectlyAssumesStringIsNiceFile15_IsTrue()
+        {
+            var instance = new AdventOfCode2015.Problems.Day5("Day5Test15.txt");
+            instance.SecondResult.Should().Be(1);
+        }
+        [TestMethod]
+        [DeploymentItem("Inputs/Day5/Day5Test16.txt")]
+        public void Part2_CorrectlyAssumesStringIsNaughtyFile16_IsTrue()
+        {
+            var instance = new AdventOfCode2015.Problems.Day5("Day5Test16.txt");
+            instance.SecondResult.Should().Be(0);
+        }
+        [TestMethod]
+        [DeploymentItem("Inputs/Day5/Day5Test17.txt")]
+        public void Part2_CorrectlyAssumesStringIsNaughtyFile17_IsTrue()
+        {
+            var instance = new AdventOfCode2015.Problems.Day5("Day5Test17.txt");
+            instance.SecondResult.Should().Be(0);
         }
     }
 }
