8770421f8f34ace011e74bd0090cd53a4d238208
CaptainCrog <craig.penning@gmail.com>
2025-01-05 15:16:30 +0000

    Implemented Day 2 solution

---
+++ CommonTypes/CommonTypes/Classes/ThreeDimensionalObject.cs
@@ -0,0 +1,50 @@
+﻿namespace CommonTypes.CommonTypes.Classes
+{
+    public class ThreeDimensionalObject
+    {
+        int _length = 0;
+        int _width = 0;
+        int _height = 0;
+
+        public int Length
+        {
+            get => _length;
+            set
+            {
+                if (_length != value)
+                {
+                    _length = value;
+                }
+            }
+        }
+
+        public int Width
+        {
+            get => _width;
+            set
+            {
+                if (_width != value)
+                {
+                    _width = value;
+                }
+            }
+        }
+        public int Height
+        {
+            get => _height;
+            set
+            {
+                if (_height != value)
+                {
+                    _height = value;
+                }
+            }
+        }
+        public ThreeDimensionalObject(int length, int width, int height) 
+        {
+            _length = length;
+            _width = width;
+            _height = height;
+        }
+    }
+}
