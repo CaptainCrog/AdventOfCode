3380f202eb2632e0ec4117a766813271d6a2cc71
CaptainCrog <craig.penning@gmail.com>
2025-01-15 23:41:19 +0000

    Removed pre-existing code

--- AdventOfCode2015Tests/Day17.cs
+++ AdventOfCode2015Tests/Day17.cs
@@ -8,15 +8,15 @@ namespace AdventOfCode2015Tests
         public void Part1_ProducesCorrectStringOutput_IsTrue()
         {
             var instance = new AdventOfCode2015.Problems.Day17("Day17Test1.txt");
-            instance.FirstResult.Should().Be("4,6,3,5,6,3,5,2,1,0");
+            instance.FirstResult.Should().Be(4);
         }
 
-        [TestMethod]
-        [DeploymentItem("Inputs/Day17/Day17Test2.txt")]
-        public void Part2_ProducesCorrectLowestInitialValue_IsTrue()
-        {
-            var instance = new AdventOfCode2015.Problems.Day17("Day17Test2.txt");
-            instance.SecondResult.Should().Be(117440);
-        }
+        //[TestMethod]
+        //[DeploymentItem("Inputs/Day17/Day17Test2.txt")]
+        //public void Part2_ProducesCorrectLowestInitialValue_IsTrue()
+        //{
+        //    var instance = new AdventOfCode2015.Problems.Day17("Day17Test2.txt");
+        //    instance.SecondResult.Should().Be(117440);
+        //}
     }
 }
