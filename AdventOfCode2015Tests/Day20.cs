a341c38db90a526f7192290e4ea210f4398f3203
CaptainCrog <craig.penning@gmail.com>
2025-01-19 16:24:42 +0000

    Removed placeholder test

--- AdventOfCode2015Tests/Day20.cs
+++ AdventOfCode2015Tests/Day20.cs
@@ -10,13 +10,5 @@ namespace AdventOfCode2015Tests
             var instance = new AdventOfCode2015.Problems.Day20("Day20Test1.txt");
             instance.FirstResult.Should().Be(6);
         }
-
-        [TestMethod]
-        [DeploymentItem("Inputs/Day20/Day20Test1.txt")]
-        public void Part2_ProducesCorrectTotalOfCheats_IsTrue()
-        {
-            var instance = new AdventOfCode2015.Problems.Day20("Day20Test1.txt");
-            instance.SecondResult.Should().Be(285);
-        }
-    }
+        //No example for Part 2 to test against
 }
