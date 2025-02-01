42feeed45c6de3e42890515bbf2b0c799fbf575c
CaptainCrog <craig.penning@gmail.com>
2025-02-01 00:43:05 +0000

    Removed commented test

--- AdventOfCode2016Tests/Day8.cs
+++ AdventOfCode2016Tests/Day8.cs
@@ -5,18 +5,12 @@ namespace AdventOfCode2016Tests
     {
         [TestMethod]
         [DeploymentItem("Inputs/Day8/Day8Test1.txt")]
-        public void Part1_OutputsValidIpAddressCountFile1_IsTrue()
+        public void Part1_OutputsCorrectTrueCount_IsTrue()
         {
             var instance = new AdventOfCode2016.Problems.Day8("Day8Test1.txt", 3, 7);
             instance.FirstResult.ResultValue.Should().Be(6);
         }
 
-        //[TestMethod]
-        //[DeploymentItem("Inputs/Day8/Day8Test5.txt")]
-        //public void Part2_OutputsSslSupportedIpAddressesFile5_IsTrue()
-        //{
-        //    var instance = new AdventOfCode2016.Problems.Day8("Day8Test5.txt");
-        //    instance.SecondResult.ResultValue.Should().Be(1);
-        //}
+        //No part 2 data to test against
     }
 }
