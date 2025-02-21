b374dd9c857b7415653ee0cbbdbde28ba92eacb9
CaptainCrog <craig.penning@gmail.com>
2025-02-21 23:10:21 +0000

    Solved Day 23

--- AdventOfCode2016Tests/Day23.cs
+++ AdventOfCode2016Tests/Day23.cs
@@ -5,10 +5,10 @@ namespace AdventOfCode2016Tests
     {
         [TestMethod]
         [DeploymentItem("Inputs/Day23/Day23Test1.txt")]
-        public void Part2_OutputsCorrectValueAtRegisterA_IsTrue()
+        public void Part1_OutputsCorrectValueAtRegisterA_IsTrue()
         {
             var instance = new AdventOfCode2016.Problems.Day23("Day23Test1.txt", 0);
-            instance.SecondResult.Should().Be(3);
+            instance.FirstResult.Should().Be(3);
         }
     }
 }
