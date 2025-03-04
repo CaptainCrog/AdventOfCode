8941a8d1c4c2b4aa5a2c0d50cc4c2651dd70e4b8
CaptainCrog <craig.penning@gmail.com>
2025-03-04 23:46:18 +0000

    Solved Part 2

--- src/AdventOfCode2017/Problems/Day07.cs
+++ src/AdventOfCode2017/Problems/Day07.cs
@@ -11,10 +11,11 @@ namespace AdventOfCode2017.Problems
         string _inputPath = string.Empty;
         string _firstResult = string.Empty;
         int _secondResult = 0;
-        List<TreeNode> _tree = new List<TreeNode>();
+        //List<TreeNode> _tree = new List<TreeNode>();
         Regex _numberRegex = CommonRegexHelpers.NumberRegex();
+        Dictionary<string, TreeNode> _tree = new();
         Dictionary<string, string> _treeLinks = new();
-        Dictionary<string, int> _treeWeights = new();
+        Dictionary<string, int> _subtreeWeights = new();
 
         #endregion
 
@@ -71,7 +72,7 @@ namespace AdventOfCode2017.Problems
                     Weight = int.Parse(_numberRegex.Match(treeDetails[1]).Value),
                     Children = treeChildren
                 };
-                _tree.Add(treeNode);
+                _tree.Add(treeNode.Name, treeNode);
             }
         }
 
@@ -83,15 +84,15 @@ namespace AdventOfCode2017.Problems
 
         public T SolveFirstProblem<T>() where T : IConvertible
         {
-            foreach(var treeNode in _tree.Where(x => x.Children.Any()))
+            foreach(var treeNode in _tree.Where(x => x.Value.Children.Any()))
             {
-                foreach(var child in treeNode.Children)
+                foreach(var child in treeNode.Value.Children)
                 {
-                    _treeLinks.Add(child, treeNode.Name);
+                    _treeLinks.Add(child, treeNode.Value.Name);
                 }
             }
             var children = _treeLinks.Select(x => x.Key).ToHashSet();
-            var allTreeElements = _tree.Select(x => x.Name).ToHashSet();
+            var allTreeElements = _tree.Select(x => x.Value.Name).ToHashSet();
             var topParent = allTreeElements.Except(children).Single();
 
             return (T)Convert.ChangeType(topParent, typeof(T));
@@ -99,17 +100,24 @@ namespace AdventOfCode2017.Problems
 
         public T SolveSecondProblem<T>() where T : IConvertible
         {
-            foreach (var treeNode in _tree.Where(x => x.Children.Any() && x.Name != FirstResult))
-            {
-                _treeWeights.Add(treeNode.Name, _tree.Where(x => treeNode.Children.Contains(x.Name)).Sum(x => x.Weight) + treeNode.Weight);
-            }
+            GetTotalWeight(FirstResult);
 
-            var groupedResults = _treeWeights.GroupBy(x => x.Value);
-            var heavyTree = groupedResults.Where(x => x.Count() == 1).Select(x => x).Single().Single();
-            var balancedTree = groupedResults.Where(x => x.Count() > 1).Select(x => x.Key).First();
-            var difference = heavyTree.Value - balancedTree;
-            var treeToEdit = _tree.Where(x => x.Name == heavyTree.Key).Single();
-            var result = treeToEdit.Weight - difference;
+            // Find the unbalanced node
+            var startNode = _tree[FirstResult];
+            string unbalancedNode = FindUnbalancedNode(startNode, _subtreeWeights);
+
+            // Find the incorrect weight adjustment
+            var unbalancedParent = _tree[_treeLinks[unbalancedNode]];
+            var correctWeight = unbalancedParent.Children
+                .GroupBy(child => _subtreeWeights[child])
+                .OrderByDescending(g => g.Count())
+                .First()
+                .Key;
+
+            int weightDifference = _subtreeWeights[unbalancedNode] - correctWeight;
+            var nodeToFix = _tree[unbalancedNode];
+
+            int result = nodeToFix.Weight - weightDifference;
 
             return (T)Convert.ChangeType(result, typeof(T));
         }
@@ -120,6 +128,32 @@ namespace AdventOfCode2017.Problems
             internal required string Name { get; init; }
             internal required int Weight { get; init; }
             internal required string[] Children { get; init; }
+        }
+        string FindUnbalancedNode(TreeNode node, Dictionary<string, int> subtreeWeights)
+        {
+            while (true)
+            {
+                var grouped = node.Children.GroupBy(child => subtreeWeights[child]).ToList();
+
+                if (grouped.Count == 1)
+                    return node.Name; // No imbalance found, return last checked node
+
+                var unbalanced = grouped.First(x => x.Count() == 1);
+                node = _tree.First(n => n.Value.Name == unbalanced.First()).Value;
+            }
+        }
+
+
+        int GetTotalWeight(string nodeName)
+        {
+            if (_subtreeWeights.ContainsKey(nodeName))
+                return _subtreeWeights[nodeName];
+
+            var node = _tree[nodeName];
+            int totalWeight = node.Weight + node.Children.Sum(GetTotalWeight);
+
+            _subtreeWeights[nodeName] = totalWeight;
+            return totalWeight;
         }
 
         #endregion
