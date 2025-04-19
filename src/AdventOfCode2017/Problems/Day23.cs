81be5b6c5eca1963f1d9c2a6de3b13ea51fb4e09
CaptainCrog <craig.penning@gmail.com>
2025-04-19 18:18:18 +0100

    Solved Part 2

--- src/AdventOfCode2017/Problems/Day23.cs
+++ src/AdventOfCode2017/Problems/Day23.cs
@@ -1,4 +1,5 @@
 ﻿using CommonTypes.CommonTypes.Interfaces;
+using System.Diagnostics.Metrics;
 
 namespace AdventOfCode2017.Problems
 {
@@ -100,37 +101,25 @@ namespace AdventOfCode2017.Problems
 
         public T SolveSecondProblem<T>() where T : IConvertible
         {
-            long result = 0;
-            Dictionary<string, long> registers = new() { { "a", 1 }, { "b", 0 }, { "c", 0 }, { "d", 0 }, { "e", 0 }, { "f", 0 }, { "g", 0 }, { "h", 0 } };
-            for (long i = 0; i < _instructions.Length; i++)
+            /// a == 1, then:
+            ///     b starts at 106700
+            ///     c ends at 123700
+            ///     
+            /// Loop between b -> c by 17
+            /// For each value of b, check if it's a prime
+            /// If b is a prime, increment h
+            /// This is done through a nested loop over d and e from 2 to b, multiplying d* e, and checking if it equals b.
+
+            long h = 0;
+            for (int b = 106700; b <= 123700; b += 17)
             {
-                var instruction = _instructions[i];
-                var parts = instruction.Split(' ');
-                var registerKey = parts[1];
-                var registerAdjustment = parts.Length == 3 ? int.TryParse(parts[2], out int _) ? int.Parse(parts[2]) : AddOrGetRegister(ref registers, parts[2]) : 0;
-                switch (parts[0])
+                if (!IsPrime(b))
                 {
-                    case "set":
-                        SetRegister(ref registers, registerKey, registerAdjustment);
-                        break;
-                    case "sub":
-                        SetRegister(ref registers, registerKey, AddOrGetRegister(ref registers, registerKey) - registerAdjustment);
-                        break;
-                    case "mul":
-                        SetRegister(ref registers, registerKey, AddOrGetRegister(ref registers, registerKey) * registerAdjustment);
-                        result++;
-                        break;
-                    case "jnz":
-                        var registerValue = int.TryParse(registerKey, out int _) ? int.Parse(registerKey) : AddOrGetRegister(ref registers, registerKey);
-                        if (registerValue != 0)
-                        {
-                            i += registerAdjustment - 1;
-                        }
-                        break;
+                    h++;
                 }
             }
 
-            return (T)Convert.ChangeType(result, typeof(T));
+            return (T)Convert.ChangeType(h, typeof(T));
         }
 
         long AddOrGetRegister(ref Dictionary<string, long> registers, string registerKey)
@@ -148,6 +137,19 @@ namespace AdventOfCode2017.Problems
         void SetRegister(ref Dictionary<string, long> registers, string registerKey, long value)
         {
             registers[registerKey] = value;
+        }
+
+        bool IsPrime(long n)
+        {
+            if (n < 2) 
+                return false;
+            int sqrt = (int)Math.Sqrt(n);
+            for (long i = 2; i <= sqrt; i++)
+            {
+                if (n % i == 0) 
+                    return false;
+            }
+            return true;
         }
         #endregion
     }
